fd834b06157daa0ca964274db54e33fce40756c1
Michael <michaelsouliman@gmail.com>
2021-10-28 23:35:46 -0700

    Initial commit

---
+++ app/src/main/java/com/msoul/mymaps/models/Place.kt
@@ -0,0 +1,7 @@
+package com.msoul.mymaps.models
+
+import java.io.Serializable
+
+data class Place(val title: String, val description: String, val latitude: Double, val longitude: Double) : Serializable {
+
+}
---
+++ app/src/main/java/com/msoul/mymaps/models/UserMap.kt
@@ -0,0 +1,7 @@
+package com.msoul.mymaps.models
+
+import java.io.Serializable
+
+data class UserMap(val title: String?, val places: List<Place>) : Serializable{
+
+}
